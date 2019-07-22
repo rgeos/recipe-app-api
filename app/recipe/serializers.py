d65d4a0997fa0ccc087e41a3e74baa4c6e9c9a93
r. geo.s <george.serban.radescu@gmail.com>
2019-07-22 18:20:49 +0900

    Created tag endpoint

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from rest_framework import serializers
+from core.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
---
+++ app/recipe/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
